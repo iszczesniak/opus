839672e74deb6c672cc19e9f554457d534438dcb
Irek Szczesniak <irek.szczesniak@gmail.com>
2011-05-27 21:01:44 +0200

    added the uniform traffic to tragen

--- utils_tragen.cc
+++ utils_tragen.cc
@@ -1,6 +1,7 @@
 #include "utils.hpp"
 #include "utils_tragen.hpp"
 
+#include <boost/foreach.hpp>
 #include <boost/graph/random.hpp>
 #include <boost/random/linear_congruential.hpp>
 
@@ -12,39 +13,49 @@ using namespace boost::random;
 void
 generate_tm(const Graph &g, fp_matrix &tm, const tragen_args &args)
 {
-  if (!args.nlimit.first)
-    generate_random_tm(g, tm, args.nr_demands.second,
-		       args.poisson_mean.second);
-  else
-    generate_random_tm(g, tm, args.nr_demands.second,
-		       args.poisson_mean.second,
-		       args.nlimit.second);
-}
+  assert(args.poisson_mean.first);
+  double p = args.poisson_mean.second;
 
-void
-generate_random_tm(const Graph &g, fp_matrix &tm, int demands,
-		   double poisson_mean)
-{
-  assert(check_components(g));
-  set<Vertex> s = *get_components(g).begin();
-  generate_random_tm(g, tm, demands, poisson_mean, s);
-}
-
-void
-generate_random_tm(const Graph &g, fp_matrix &tm, int demands,
-		   double poisson_mean, int n)
-{
+  // The set of vertices between whom the demands will be created.
   set<Vertex> s;
-  for(int i = 0; i < n; ++i)
-    s.insert(i);
-  generate_random_tm(g, tm, demands, poisson_mean, s);
+
+  if (args.nlimit.first)
+    {
+      int n = args.nlimit.second;
+      for(int i = 0; i < n; ++i)
+	s.insert(i);
+    }
+  else
+    {
+      assert(check_components(g));
+      s = *get_components(g).begin();
+    }
+
+  switch(args.tt.second)
+    {
+    case tragen_args::random:
+      {
+	assert(args.nr_demands.first);
+	int d = args.nr_demands.second;
+	generate_random_tm(g, tm, d, p, s);
+	break;
+      }
+
+    case tragen_args::uniform:
+      generate_uniform_tm(g, tm, p, s);
+      break;
+
+    default:
+      assert(false);
+    }
 }
 
 void
 generate_random_tm(const Graph &g, fp_matrix &tm, int demands,
-		   double poisson_mean, set<Vertex> s)
+		   double poisson_mean, const set<Vertex> &st)
 {
   minstd_rand gen;
+  set<Vertex> s = st;
 
   for(int d = 0; d < demands; ++d)
     {
@@ -55,4 +66,14 @@ generate_random_tm(const Graph &g, fp_matrix &tm, int demands,
 
       tm[dst][src] += poisson_mean;
     }
+}
+
+void
+generate_uniform_tm(const Graph &g, fp_matrix &tm, double poisson_mean,
+		    const set<Vertex> &s)
+{
+  BOOST_FOREACH(Vertex src, s)
+    BOOST_FOREACH(Vertex dst, s)
+      if (src != dst)
+        tm[dst][src] = poisson_mean;
 }
--- utils_tragen.hpp
+++ utils_tragen.hpp
@@ -12,24 +12,17 @@ void
 generate_tm(const Graph &g, fp_matrix &tm, const tragen_args &args);
 
 /**
- * Generate the random traffic matrix.
+ * Generate the random traffic matrix between the vertexes in the given set.
  */
 void
 generate_random_tm(const Graph &g, fp_matrix &tm, int demands,
-		   double poisson_mean);
-
-/**
- * Generate the random traffic matrix.
- */
-void
-generate_random_tm(const Graph &g, fp_matrix &tm, int demands,
-		   double poisson_mean, int n);
+		   double poisson_mean, const set<Vertex> &s);
 
 /**
  * Generate the random traffic matrix between the vertexes in the given set.
  */
 void
-generate_random_tm(const Graph &g, fp_matrix &tm, int demands,
-		   double poisson_mean, set<Vertex> s);
+generate_uniform_tm(const Graph &g, fp_matrix &tm, double poisson_mean,
+		    const set<Vertex> &s);
 
 #endif /* UTILS_TRAGEN_HPP */
